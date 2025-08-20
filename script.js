fcf8f4a6ee0c2b23c0406e2a94de9c2af2e80fa9
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2025-08-20 13:46:44 -0400

    Script time that updates in real time for header

---
+++ script.js
@@ -0,0 +1,18 @@
+setInterval(showTime, 1000);
+
+function showTime(){
+    let time = new Date;
+    // let month = time.getMonth();
+    // let day = time.getDay();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
+
+    let timeStr = hour + ":" + min + ":" + sec;
+    document.getElementById('section__time').innerHTML = timeStr;
+}
+
+showTime();
