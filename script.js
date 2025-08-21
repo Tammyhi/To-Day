bfaefea66c76b18ecf30b787c007fb1b86bfd584
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2025-08-21 14:02:42 -0400

    Update time to show month, day, and date

--- script.js
+++ script.js
@@ -13,6 +13,80 @@ function showTime(){
 
     let timeStr = hour + ":" + min + ":" + sec;
     document.getElementById('header__time').innerHTML = timeStr;
+    // Add thing that when it hits midnight call showDate again
 }
 
+function showDate(){
+    let date = new Date;
+    let month = date.getMonth();
+    let dayOfWeek = date.getDay();
+    let dayOfMonth = date.getDate();
+
+    switch(month) {
+        case 0:
+            month = "Jan";
+            break;
+        case 1:
+            month = "Feb";
+            break;
+        case 2:
+            month = "Mar";
+            break;
+        case 3:
+            month = "Apr";
+            break;
+        case 4:
+            month = "May";
+            break;
+        case 5:
+            month = "Jun";
+            break;
+        case 6:
+            month = "Jul";
+            break;
+        case 7:
+            month = "Aug";
+            break;
+        case 8:
+            month = "Sep";
+            break;
+        case 9:
+            month = "Oct";
+            break;
+        case 10:
+            month = "Nov";
+            break;
+        case 11:
+            month = "Dec";
+            break;
+    }
+
+    switch(dayOfWeek) {
+        case 0:
+            dayOfWeek = "Sun";
+            break;
+        case 1:
+            dayOfWeek = "Mon";
+            break;
+        case 2:
+            dayOfWeek = "Tue";
+            break;
+        case 3:
+            dayOfWeek = "Wed";
+            break;
+        case 4:
+            dayOfWeek = "Thu";
+            break;
+        case 5:
+            dayOfWeek = "Fri";
+            break;
+        case 6:
+            dayOfWeek = "Sat";
+            break;
+    }
+
+    let dateStr = dayOfWeek + " " + month + " " + dayOfMonth;
+    document.getElementById('header__date').innerHTML = dateStr;
+}
 showTime();
+showDate();
