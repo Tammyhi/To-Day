5c8a8592e9e9c36f1a89decb69a1720c70eff6e8
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2025-12-21 17:41:30 -0500

    refactor: Add task to localStorage on add + update task desc in localStorage if changed

--- script.js
+++ script.js
@@ -28,14 +28,21 @@ document.addEventListener('DOMContentLoaded',() => {
                 const taskListBtnIcon = taskListBtn.querySelector('.task-list__btn__icon--incomplete');
                 taskListBtnIcon.classList.remove('task-list__btn__icon--incomplete','ph');
                 taskListBtnIcon.classList.add('task-list__btn__icon--completed','ph-fill');
+                tasks[taskListItem.id].status = "complete";
+                localStorage.setItem(taskListItem.id, JSON.stringify(tasks[taskListItem.id]));
             }
         }
         else if(target.closest('.task-list__item__desc')){
+           //const taskListItem = event.target.closest('.task-list__item--incomplete');
             let newDesc = target.closest('.task-list__item__desc');
             newDesc.addEventListener('keydown', (event) => {
                 if(event.key === 'Enter'){
                     event.preventDefault();
                 }
+            })
+            newDesc.addEventListener('blur', (event) => {
+                tasks[newDesc.parentElement.id].desc = newDesc.innerHTML;
+                localStorage.setItem(newDesc.parentElement.id, JSON.stringify(tasks[newDesc.parentElement.id]));
             })
         }
     });
@@ -56,6 +63,8 @@ document.addEventListener('DOMContentLoaded',() => {
                 const completedListBtnIcon = completedListBtn.querySelector('.task-list__btn__icon--completed');
                 completedListBtnIcon.classList.remove('task-list__btn__icon--completed','ph-fill');
                 completedListBtnIcon.classList.add('task-list__btn__icon--incomplete','ph');
+                tasks[completedListItem.id].status = "incomplete";
+                localStorage.setItem(completedListItem.id, tasks[completedListItem.id]);
             }
         }
         else if(target.closest('.task-list__item__desc')){
@@ -64,6 +73,10 @@ document.addEventListener('DOMContentLoaded',() => {
                 if(event.key === 'Enter'){
                     event.preventDefault();
                 }
+            })
+            newDesc.addEventListener('blur', (event) => {
+                tasks[newDesc.parentElement.id].desc = newDesc.innerHTML;
+                localStorage.setItem(newDesc.parentElement.id, JSON.stringify(tasks[newDesc.parentElement.id]));
             })
         }
     });
@@ -102,12 +115,11 @@ document.addEventListener('DOMContentLoaded',() => {
             if (addTaskDesc.value.trim() !== ''){
                 const newTask = createTaskElement(addTaskDesc.value);
                 taskList.appendChild(newTask);
-                // tasks.push({
-                //     id: taskCounter++,
-                //     desc: addTaskDesc.value,
-                //     status: "incomplete",
-                // });
-                saveData(tasks);
+                tasks.push({
+                     desc: addTaskDesc.value,
+                     status: "incomplete",
+                 });
+                localStorage.setItem(tasks.length - 1, JSON.stringify(tasks[tasks.length - 1]));
                 addTaskDesc.value = '';
             }
         }
@@ -117,168 +129,156 @@ document.addEventListener('DOMContentLoaded',() => {
         if (addTaskDesc.value.trim() !== ''){
                 const newTask = createTaskElement(addTaskDesc.value);
                 taskList.appendChild(newTask);
-                // tasks.push({
-                //     id: taskCounter++,
-                //     desc: addTaskDesc.value,
-                //     status: "incomplete",
-                // });
-                saveData(tasks);
+                tasks.push({
+                     desc: addTaskDesc.value,
+                     status: "incomplete",
+                 });
+                localStorage.setItem(tasks.length - 1, JSON.stringify(tasks[tasks.length - 1]));
                 addTaskDesc.value = '';
             }
     })
+
+        
+    // return created task element 
+    function createTaskElement(desc, status){
+        const taskEl = document.createElement('div');
+        taskEl.classList.add('task-list__item','task-list__item--' + status);
+
+        const taskBtn = document.createElement('button');
+        taskBtn.classList.add('task-list__btn', 'task-list__btn--' + status);
+
+        const taskBtnIcon = document.createElement('i');
+        taskBtnIcon.classList.add('ph','ph-circle', 'task-list__btn__icon','task-list__btn__icon--' + status);
+
+        const taskDesc = document.createElement('p');
+        taskDesc.classList.add('task-list__item__desc');
+        taskDesc.setAttribute("contenteditable", "true");
+        taskDesc.setAttribute("spellcheck", "false");
+        taskDesc.innerHTML = desc;
+
+        taskBtn.appendChild(taskBtnIcon);
+        taskEl.appendChild(taskBtn);
+        taskEl.appendChild(taskDesc);
+        taskEl.setAttribute('id', tasks.length);
+        return taskEl;
+    }
+
+    function showTime(){
+        let time = new Date;
+        // let month = time.getMonth();
+        // let day = time.getDay();
+        let hour = time.getHours();
+        let min = time.getMinutes();
+        let sec = time.getSeconds();
+
+        min = min < 10 ? '0' + min : min;
+        sec = sec < 10 ? '0' + sec : sec;
+
+        let timeStr = hour + ":" + min + ":" + sec;
+        document.getElementById('header__time').innerHTML = timeStr;
+        // Add thing that when it hits midnight call showDate again
+    }
+
+    function showDate(){
+        let date = new Date;
+        let month = date.getMonth();
+        let dayOfWeek = date.getDay();
+        let dayOfMonth = date.getDate();
+
+        switch(month) {
+            case 0:
+                month = "Jan";
+                break;
+            case 1:
+                month = "Feb";
+                break;
+            case 2:
+                month = "Mar";
+                break;
+            case 3:
+                month = "Apr";
+                break;
+            case 4:
+                month = "May";
+                break;
+            case 5:
+                month = "Jun";
+                break;
+            case 6:
+                month = "Jul";
+                break;
+            case 7:
+                month = "Aug";
+                break;
+            case 8:
+                month = "Sep";
+                break;
+            case 9:
+                month = "Oct";
+                break;
+            case 10:
+                month = "Nov";
+                break;
+            case 11:
+                month = "Dec";
+                break;
+        }
+
+        switch(dayOfWeek) {
+            case 0:
+                dayOfWeek = "Sun";
+                break;
+            case 1:
+                dayOfWeek = "Mon";
+                break;
+            case 2:
+                dayOfWeek = "Tue";
+                break;
+            case 3:
+                dayOfWeek = "Wed";
+                break;
+            case 4:
+                dayOfWeek = "Thu";
+                break;
+            case 5:
+                dayOfWeek = "Fri";
+                break;
+            case 6:
+                dayOfWeek = "Sat";
+                break;
+        }
+
+        let dateStr = dayOfWeek + " " + month + " " + dayOfMonth;
+        document.getElementById('header__date').innerHTML = dateStr;
+    }
+
+    function loadData(){
+        if (localStorage.length > 0){
+            for(let i = 0; i < localStorage.length; i++){
+                let taskEl = localStorage.getItem(localStorage.key(i));
+                taskEl = JSON.parse(taskEl);
+                tasks.push(taskEl);
+                let elStatus = taskEl.status;
+                let elDesc = taskEl.desc;
+                let newEl = createTaskElement(elStatus, elDesc);
+                if(elStatus === "incomplete"){
+                    taskList.appendChild(newEl);
+                }
+                else{
+                    completedList.appendChild(newEl);
+                }
+            }
+        }
+    }
+
+    function midnightReset(){
+        localStorage.clear();
+    }
 
     // update clock
     setInterval(showTime, 1000);
     showTime();
     showDate();
-
-    loadData(tasks);
-});
-
-// return created task element 
-function createTaskElement(desc){
-    const taskEl = document.createElement('div');
-    taskEl.classList.add('task-list__item','task-list__item--incomplete');
-
-    const taskBtn = document.createElement('button');
-    taskBtn.classList.add('task-list__btn', 'task-list__btn--incomplete');
-
-    const taskBtnIcon = document.createElement('i');
-    taskBtnIcon.classList.add('ph','ph-circle', 'task-list__btn__icon','task-list__btn__icon--incomplete');
-
-    const taskDesc = document.createElement('p');
-    taskDesc.classList.add('task-list__item__desc');
-    taskDesc.setAttribute("contenteditable", "true");
-    taskDesc.setAttribute("spellcheck", "false");
-    taskDesc.innerHTML = desc;
-
-    taskBtn.appendChild(taskBtnIcon);
-    taskEl.appendChild(taskBtn);
-    taskEl.appendChild(taskDesc);
-    return taskEl;
-}
-
-function showTime(){
-    let time = new Date;
-    // let month = time.getMonth();
-    // let day = time.getDay();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-
-    min = min < 10 ? '0' + min : min;
-    sec = sec < 10 ? '0' + sec : sec;
-
-    let timeStr = hour + ":" + min + ":" + sec;
-    document.getElementById('header__time').innerHTML = timeStr;
-    // Add thing that when it hits midnight call showDate again
-}
-
-function showDate(){
-    let date = new Date;
-    let month = date.getMonth();
-    let dayOfWeek = date.getDay();
-    let dayOfMonth = date.getDate();
-
-    switch(month) {
-        case 0:
-            month = "Jan";
-            break;
-        case 1:
-            month = "Feb";
-            break;
-        case 2:
-            month = "Mar";
-            break;
-        case 3:
-            month = "Apr";
-            break;
-        case 4:
-            month = "May";
-            break;
-        case 5:
-            month = "Jun";
-            break;
-        case 6:
-            month = "Jul";
-            break;
-        case 7:
-            month = "Aug";
-            break;
-        case 8:
-            month = "Sep";
-            break;
-        case 9:
-            month = "Oct";
-            break;
-        case 10:
-            month = "Nov";
-            break;
-        case 11:
-            month = "Dec";
-            break;
-    }
-
-    switch(dayOfWeek) {
-        case 0:
-            dayOfWeek = "Sun";
-            break;
-        case 1:
-            dayOfWeek = "Mon";
-            break;
-        case 2:
-            dayOfWeek = "Tue";
-            break;
-        case 3:
-            dayOfWeek = "Wed";
-            break;
-        case 4:
-            dayOfWeek = "Thu";
-            break;
-        case 5:
-            dayOfWeek = "Fri";
-            break;
-        case 6:
-            dayOfWeek = "Sat";
-            break;
-    }
-
-    let dateStr = dayOfWeek + " " + month + " " + dayOfMonth;
-    document.getElementById('header__date').innerHTML = dateStr;
-}
-
-// call after tasks is updated (add task, del task, edit task desc, edit task status, etc.)
-// later alter so can save color scheme n other stuff
-function saveData(dataToSave){
-    localStorage.setItem("taskItems", JSON.stringify(dataToSave));
-}
-
-function loadData(tasksEl){
-    const savedTasks = localStorage.getItem("taskItems");
-    if (savedTasks){
-        tasksEl = JSON.parse(savedTasks);
-        renderTasks(savedTasks);
-    }
-    else{
-        localStorage.setItem("taskItems","");
-    }
-}
-
-function renderTasks(tasksToRender){
-    const renderToDo = tasksToRender.filter((task) => task.status === incomplete);
-    const renderToCelebrate = tasksToRender.filter((task) => task.status === completed);
-
-    document.getElementById("task-list")
-}
-
-function midnightReset(){
     localStorage.clear();
-}
-
-/* In local storage
-    During the day store the current tasks in the completed list and incomplete list, git sta
-    create object for each list, counter.
-    when complete task, incomplete task, add task, edit task
-
-    localStorage.setitem(keystr, valstr)
-    */
+    loadData();
+});
